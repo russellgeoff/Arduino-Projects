4627b5e502ca00fe4c76199f8438c29d9574bcbf
Geoff Russell <Geoff.R.Russell@gmail.com>
2015-07-30 09:28:18 -0700

    Tries to set time until it receives a confirmation back from the arduino

--- LCD_Serial_Display/arduinoserial.py
+++ LCD_Serial_Display/arduinoserial.py
@@ -4,10 +4,15 @@ import time
 ser = serial.Serial(4, 9600)
 
 print ser.name
-time.sleep(.5)
-timestamp = int(time.time())
-messageStr = "T" + str(timestamp) + "000"
-print messageStr
-ser.write(messageStr)
+
+while ("I got a time!" not in ser.readline()):
+    timestamp = int(time.time())
+    messageStr = "T" + str(timestamp) + "000"
+    print messageStr
+    ser.write(messageStr)
+    time.sleep(1)
+    # output = ser.readline()
+    # print(output)
+    # print("I got a time!" not in output)
 
 ser.close()
